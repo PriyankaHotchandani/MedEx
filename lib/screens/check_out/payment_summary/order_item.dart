10bee4a4f658c477052485fc7dbc3e45e32aa826
Priyanka Hotchandani <68430517+PriyankaHotchandani@users.noreply.github.com>
2021-11-17 19:58:19 +0530

    files

---
+++ lib/screens/check_out/payment_summary/order_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:med_x/models/review_cart_model.dart';
+import 'package:med_x/providers/review_cart_provider.dart';
+
+class OrderItem extends StatelessWidget {
+  final ReviewCartModel e;
+  OrderItem({this.e});
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.network(
+        e.cartImage,
+        width: 60,
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+           e.cartName,
+            style: TextStyle(
+              color: Colors.grey[600],
+            ),
+          ),
+          Text(
+            e.cartUnit,
+            style: TextStyle(
+              color: Colors.grey[600],
+            ),
+          ),
+          Text(
+            "\$${e.cartPrice}",
+            
+          ),
+        ],
+      ),
+      subtitle: Text(e.cartQuantity.toString()),
+    );
+  }
+}
